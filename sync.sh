2fbca17e5c01d452ac53dc5abe50d60bde6b79f2
Luo Tao <lotreal@gmail.com>
2018-04-11 08:49:38 +0800

    clean failed images

--- sync.sh
+++ sync.sh
@@ -1,25 +1,18 @@
 #!/usr/bin/env bash
-read -r -d '' IMAGES <<- EOM
-gcr.io/spinnaker-marketplace/clouddriver:2.0.0-20180221152902
-gcr.io/spinnaker-marketplace/echo:0.8.0-20180221133510
-gcr.io/spinnaker-marketplace/deck:2.1.0-20180221143146
-gcr.io/spinnaker-marketplace/igor:0.9.0-20180221133510
-gcr.io/spinnaker-marketplace/orca:0.10.0-20180221133510
-gcr.io/spinnaker-marketplace/gate:0.10.0-20180221133510
-gcr.io/spinnaker-marketplace/front50:0.9.0-20180221133510
-gcr.io/spinnaker-marketplace/rosco:0.5.0-20180221133510
-gcr.io/spinnaker-marketplace/halyard:stable
-gcr.io/spinnaker-marketplace/clouddriver:stable
-gcr.io/spinnaker-marketplace/echo:stable
-gcr.io/spinnaker-marketplace/deck:stable
-gcr.io/spinnaker-marketplace/igor:stable
-gcr.io/spinnaker-marketplace/orca:stable
-gcr.io/spinnaker-marketplace/gate:stable
-gcr.io/spinnaker-marketplace/front50:stable
-gcr.io/spinnaker-marketplace/rosco:stable
-EOM
+IMAGES=(
+    gcr.io/spinnaker-marketplace/clouddriver:2.0.0-20180221152902
+    gcr.io/spinnaker-marketplace/echo:0.8.0-20180221133510
+    gcr.io/spinnaker-marketplace/deck:2.1.0-20180221143146
+    gcr.io/spinnaker-marketplace/igor:0.9.0-20180221133510
+    gcr.io/spinnaker-marketplace/orca:0.10.0-20180221133510
+    gcr.io/spinnaker-marketplace/gate:0.10.0-20180221133510
+    gcr.io/spinnaker-marketplace/front50:0.9.0-20180221133510
+    gcr.io/spinnaker-marketplace/rosco:0.5.0-20180221133510
 
-rename() {
+    gcr.io/spinnaker-marketplace/halyard:stable
+)
+
+sync() {
     ORIGIN=$1
     NEWNAME=$ORIGIN
     NEWNAME=${NEWNAME/gcr.io\//cncr*}
@@ -30,7 +23,6 @@ rename() {
     docker pull $IMAGE && docker tag $IMAGE $NEWNAME && docker push $NEWNAME
 }
 
-for IMAGE in $IMAGES
-do
-    rename $IMAGE
+for IMAGE in ${IMAGES[@]} ; do
+    sync $IMAGE
 done
